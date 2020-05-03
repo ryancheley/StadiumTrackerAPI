8ddce7b30e55c7b676c0b2a55f637aea6a35daec
Ryan Cheley <rcheley@gmail.com>
2020-05-03 09:08:36 -0700

    updated urls.py for drf-yasg

--- StadiumTrackerAPI/urls.py
+++ StadiumTrackerAPI/urls.py
@@ -1,8 +1,22 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
-from users.forms import CustomUserCreationForm
-from django_registration.backends.activation.views import RegistrationView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('stadium/', include('stadium_tracker.urls')),
@@ -11,5 +25,7 @@ urlpatterns = [
     path(r'accounts/', include('allauth.urls')),
     path(r'api/', include('api.urls')),
     path('', include('content.urls')),
-
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
