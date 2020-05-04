c818c71f30af0b4906b8674ea52d1df3c8a6268c
Ryan Cheley <rcheley@gmail.com>
2020-05-03 19:08:16 -0700

    updated test so that user doesn't require a favorite team anymore

--- users/tests/tests_views.py
+++ users/tests/tests_views.py
@@ -7,7 +7,7 @@ from users.models import CustomUser
 class UserUpdateViewTest(TestCase):
 
     def test_update_book(self):
-        user = CustomUser.objects.create(username='test', favorite_team=119)
+        user = CustomUser.objects.create(username='test')
 
         response = self.client.post(
             reverse('users:user', kwargs={'pk': user.id}),
