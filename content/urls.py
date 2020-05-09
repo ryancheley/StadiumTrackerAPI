500a51b846aaebad4f640b39d8e79eca502873e0
Ryan Cheley <rcheley@gmail.com>
2020-05-09 12:09:58 -0700

    completed migration to DetailView from TemplateView

--- content/urls.py
+++ content/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from content.views import ContentTemplateView, ContentDetailView
+from content.views import ContentRedirectView, ContentDetailView
 
 app_name = 'content'
 
 
 urlpatterns = [
-    path('', ContentTemplateView.as_view(template_name='page.html'), name='home'),
+    path('', ContentRedirectView.as_view(url='Pages/Home'), name='home'),
     path('Pages/<slug:title>', ContentDetailView.as_view(template_name='page.html'), name='content_detail'),
 ]
--- content/views.py
+++ content/views.py
@@ -1,26 +1,13 @@
 # TODO: convert Home Page from TemplateView to DetailView
 from datetime import datetime
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import DetailView, RedirectView
 from content.models import Content
-from stadium_tracker.models import GameDetails
-from stadium_tracker.game_details import get_games_for_date
 
 
-class ContentTemplateView(TemplateView):
-    model = Content
-    template_name = 'page.html'
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-
-        if 'name' in self.kwargs.keys():
-            title = self.kwargs['name']
-        else:
-            title = 'Stadia Tracker'
-        data['pages'] = Content.objects.get(title=title)
-        data['games'] = GameDetails.objects.all().order_by('-create_date')[:3]
-        data['today'] = get_games_for_date(1, datetime.strftime(datetime.now(), '%Y-%m-%d'))
-        return data
+class ContentRedirectView(RedirectView):
+    permanent = True
+    query_string = True
+    pattern_name = 'home'
 
 
 class ContentDetailView(DetailView):
@@ -29,12 +16,3 @@ class ContentDetailView(DetailView):
     slug_field = 'title'
     slug_url_kwarg = 'title'
     context_object_name = 'pages'
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-
-        data['games'] = GameDetails.objects.all().order_by('-create_date')[:3]
-        data['today'] = get_games_for_date(1, datetime.strftime(datetime.now(), '%Y-%m-%d'))
-        return data
-
-
