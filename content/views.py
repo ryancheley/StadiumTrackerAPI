8fd54e921d84c18fa553f9ffa23a02eb2947c2dd
Ryan Cheley <rcheley@gmail.com>
2020-05-09 12:14:31 -0700

    added the rhs content back to page template

--- content/views.py
+++ content/views.py
@@ -2,6 +2,8 @@
 from datetime import datetime
 from django.views.generic import DetailView, RedirectView
 from content.models import Content
+from stadium_tracker.models import GameDetails
+from stadium_tracker.game_details import get_games_for_date
 
 
 class ContentRedirectView(RedirectView):
@@ -16,3 +18,9 @@ class ContentDetailView(DetailView):
     slug_field = 'title'
     slug_url_kwarg = 'title'
     context_object_name = 'pages'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['games'] = GameDetails.objects.all().order_by('-create_date')[:3]
+        data['today'] = get_games_for_date(1, datetime.strftime(datetime.now(), '%Y-%m-%d'))
+        return data
