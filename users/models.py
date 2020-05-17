08adb46424af94ccbe6ff81715e3318c9cc151c4
Ryan Cheley <rcheley@gmail.com>
2020-05-17 08:23:40 -0700

    clean up of removal of MLB-Stats API

--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-import statsapi
+from stadium_tracker.game_details import get_teams
 
 
 class CustomUser(AbstractUser):
     team_choices = []
-    teams = statsapi.lookup_team('', sportIds=1)
+    teams = get_teams(1)
     for t in teams:
         team_choices.append((t.get("id"), t.get("name")))
     favorite_team = models.IntegerField(choices=team_choices, blank=True, null=True,)
