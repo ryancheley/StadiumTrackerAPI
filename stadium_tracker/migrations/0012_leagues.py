e44780bb0f437e28496eb3fb5d6a1c9a032c2992
Ryan Cheley <rcheley@gmail.com>
2020-05-21 18:14:46 -0700

    update to .gitignore for sql files

--- stadium_tracker/migrations/0012_leagues.py
+++ stadium_tracker/migrations/0012_leagues.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Leagues',
+            name='League',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('league_name', models.CharField(max_length=255)),
