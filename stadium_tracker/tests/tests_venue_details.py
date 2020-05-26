0c5d524a7b6ed04cae778082449d032b6708a9d7
Ryan Cheley <rcheley@gmail.com>
2020-05-25 20:01:31 -0700

    removing files no longer needed

--- stadium_tracker/tests/tests_venue_details.py
+++
@@ -1,9 +0,0 @@
-# TODO: add tests for get_venue_total
-from django.test import TestCase
-from stadium_tracker.venue_details import get_venue_details
-
-
-class TestVenueDetails(TestCase):
-    def test_get_venue_details(self):
-        x = get_venue_details(22)
-        self.assertEqual(x, "Dodger Stadium")
